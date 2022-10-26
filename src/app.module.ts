4b34c6599fc5cab7a6c0e6256dcb229166241696
hg-ian-choi <hg.ian.choi@gmail.com>
2022-10-26 22:06:12 +0900

    install typeorm pg and connect postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 // app.module.ts
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardsModule } from './boards/boards.module';
+import { typeOrmConfig } from './configs/typeorm.config';
 
 @Module({
-  imports: [BoardsModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), BoardsModule],
 })
 export class AppModule {}
