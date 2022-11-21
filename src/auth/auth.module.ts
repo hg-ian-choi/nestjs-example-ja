c3b3ac2e0631a780b7014c330f8b429998af23e5
hg-ian-choi <hg.ian.choi@gmail.com>
2022-11-21 22:47:30 +0900

    modify configs

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,14 +6,17 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './jwt.strategy';
 import { UserRepository } from './user.repository';
+import * as config from 'config';
+
+const jwtConfig = config.get('jwt');
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([UserRepository]),
     JwtModule.register({
-      secret: 'JwtSecretKey',
+      secret: process.env.JWT_SECRET_KEY || jwtConfig.jwtSecretKey,
       signOptions: {
-        expiresIn: 3600,
+        expiresIn: process.env.JWT_EXPIRESIN || jwtConfig.expiresIn,
       },
     }),
     PassportModule.register({ defaultStrategy: 'jwt' }),
