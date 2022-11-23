5231144cb4d5deb94a941a8b1c5925c99da6934a
hg-ian-choi <hg.ian.choi@gmail.com>
2022-11-23 11:01:13 +0900

    change repository structure

--- src/auth/get-user.decorator.ts
+++
@@ -1,9 +0,0 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { User } from './user.entity';
-
-export const GetUser = createParamDecorator(
-  (_data, _ctx: ExecutionContext): User => {
-    const req = _ctx.switchToHttp().getRequest();
-    return req.user;
-  },
-);
